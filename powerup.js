6e8c730dd7cd5683148da6fe915bf53b427b9a0a
Novit-Media <kontakt@novit-media.pl>
2025-07-22 14:41:49 +0200

    Add files via upload
    Some visual updates and new features

--- powerup.js
+++ powerup.js
@@ -1,6 +1,12 @@
 /* global TrelloPowerUp */
 var ICON = 'https://novit-media.github.io/time-tracker-nm-trello/icon.png';
 
+function makeAsciiBar(percent){
+  var blocks = Math.round(percent/10);
+  var bar = '█'.repeat(blocks) + '░'.repeat(10-blocks);
+  return bar + ' ' + Math.round(percent) + '%';
+}
+
 TrelloPowerUp.initialize({
   'board-buttons': function(t){
     return [{
@@ -32,19 +38,77 @@ TrelloPowerUp.initialize({
     }];
   },
   'card-detail-badges': function(t, opts){
-    return [{
-      text: '⏱️ Time Tracker',
-      icon: ICON,
-      callback: function(t){
-        return t.modal({
-          title: 'Time Tracker NM',
-          url: 'popup.html',
-          accentColor: '#667eea',
-          fullscreen: true,
-          args: { cardId: opts.context.card }
+    return t.get('card','shared','timeData',{ plannedMinutes:0, spentMinutes:0, timerRunning:false, timerStart:0 })
+      .then(function(d){
+        var planned = d.plannedMinutes || 0;
+        var spent = d.spentMinutes || 0;
+        var running = d.timerRunning || false;
+        var badges = [];
+
+        // info badge
+        badges.push({
+          text: '⏱ ' + (Math.floor(spent/60)+'h '+(spent%60)+'m') + ' / ' + (Math.floor(planned/60)+'h '+(planned%60)+'m'),
+          icon: ICON
         });
-      }
-    }];
+
+        // start / stop badge
+        if(!running){
+          badges.push({
+            text: 'Start',
+            icon: ICON,
+            callback: function(t){
+              return t.get('card','shared','timeData',{ plannedMinutes:0, spentMinutes:0 }).then(function(data){
+                data.timerRunning = true;
+                data.timerStart = Date.now();
+                return t.set('card','shared','timeData', data);
+              }).then(function(){ t.closePopup(); });
+            }
+          });
+        }else{
+          badges.push({
+            text: 'Stop',
+            icon: ICON,
+            callback: function(t){
+              return t.get('card','shared','timeData',{ plannedMinutes:0, spentMinutes:0, timerRunning:false, timerStart:0 })
+              .then(function(data){
+                var now = Date.now();
+                var diffMin = Math.ceil((now - (data.timerStart||now)) / 60000);
+                data.spentMinutes = (data.spentMinutes||0) + diffMin;
+                data.timerRunning = false;
+                data.timerStart = 0;
+                return t.set('card','shared','timeData', data).then(function(){
+                  if(typeof t.comment === 'function'){
+                    return t.comment('Automatycznie dodano '+diffMin+' minut po zatrzymaniu timera.').catch(function(){});
+                  }
+                });
+              }).then(function(){ t.closePopup(); });
+            }
+          });
+        }
+        return badges;
+      });
+  },
+  'card-badges': function(t){
+    return t.get('card','shared','timeData',{plannedMinutes:0, spentMinutes:0})
+      .then(function(d){
+        var badges = [];
+        var planned = d.plannedMinutes || 0;
+        var spent = d.spentMinutes || 0;
+
+        if(planned){
+          var ph = Math.floor(planned/60), pm = planned % 60;
+          badges.push({ text: 'Plan: ' + (ph? (ph+':'+pm.toString().padStart(2,'0')+'h') : pm+'m'), color: 'blue' });
+        }
+        if(spent){
+          var sh = Math.floor(spent/60), sm = spent % 60;
+          badges.push({ text: 'Spent: ' + (sh? (sh+':'+sm.toString().padStart(2,'0')+'h') : sm+'m'), color: 'green' });
+        }
+        if(planned>0){
+          var percent = Math.min(100, (spent/planned)*100);
+          badges.push({ text: makeAsciiBar(percent) });
+        }
+        return badges;
+      });
   },
   'card-back-section': function(t, opts){
     return {
@@ -53,22 +117,11 @@ TrelloPowerUp.initialize({
       content: { type: 'iframe', url: t.signUrl('popup.html'), height: 900 }
     };
   },
-  'card-badges': function(t){
-    return t.get('card','shared','timeData',{plannedMinutes:0, spentMinutes:0})
-    .then(function(d){
-      var badges=[];
-      if(d.plannedMinutes){
-        var ph=Math.floor(d.plannedMinutes/60), pm=d.plannedMinutes%60;
-        badges.push({text:'⏱️ '+(ph?ph+':'+pm.toString().padStart(2,'0')+'h':pm+'m'), color:'blue'});
-      }
-      if(d.spentMinutes){
-        var sh=Math.floor(d.spentMinutes/60), sm=d.spentMinutes%60;
-        badges.push({text:'✅ '+(sh?sh+':'+sm.toString().padStart(2,'0')+'h':sm+'m'), color:'green'});
-      }
-      return badges;
-    });
-  },
   'show-settings': function(t){
-    return t.popup({ title:'Time Tracker NM - Ustawienia', url:'settings.html', height:420 });
+    return t.popup({
+      title: 'Time Tracker NM - Ustawienia',
+      url: 'settings.html',
+      height: 420
+    });
   }
 });
